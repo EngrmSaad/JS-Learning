a14bee5b894a40739d90b45a5fa4099e090ddb46
EngrmSaad <engr.muhammadsaad2016@gmail.com>
2023-10-23 12:50:49 +0000

    Basics variables Done

---
+++ 01_Basics/01_variables.js
@@ -0,0 +1,8 @@
+const accountId = 1
+let accountEmail = "engr.muhammadsaad2016@gmail.com"
+var accountPassword = "12345"
+accountCity = "Karachi"
+let accountState
+// accountId = 2
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
