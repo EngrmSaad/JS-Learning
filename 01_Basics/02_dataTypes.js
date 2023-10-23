cabac560e9d389c88f919a28284f2fb09fc1bd2d
EngrmSaad <engr.muhammadsaad2016@gmail.com>
2023-10-23 13:27:18 +0000

    datatypes

---
+++ 01_Basics/02_dataTypes.js
@@ -0,0 +1,15 @@
+"use strict"; // treat all JS code as newer version
+
+// alert(3+3)  // We are using NodeJS not browser
+
+/*
+Premitive dataTypes
+
+1. Number => 2 to power 53
+2. BigInt
+3. String => ""
+4. Boolean => True/False
+5. null => Standalone value 
+6. undefined => variable not initiallized
+7. symbol => unique
+*/
