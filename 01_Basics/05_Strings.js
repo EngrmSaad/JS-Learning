0fdfdc22a4dba4df42355d9c54e62ebeffc699e6
EngrmSaad <engr.muhammadsaad2016@gmail.com>
2023-10-26 09:24:22 +0000

    numsAndMaths

---
+++ 01_Basics/05_Strings.js
@@ -0,0 +1,7 @@
+const myName = "Saad"
+let myAge = 30
+
+// Concatinate
+console.log("Hello I am " + myName + " my age is " + myAge);
+
+console.log(`Hello I am ${myName} My Age is ${myAge}`)
---
+++ 01_Basics/06_numsAndMaths.js
@@ -0,0 +1,9 @@
+let balance = new Number(100)
+
+console.log(balance);
+console.log(typeof balance);
+
+balance = "Saad"
+
+console.log(balance);
+console.log(typeof balance);
